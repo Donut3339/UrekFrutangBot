5bbe942b0a82cb850c2b04f027a29c278a8818f7
Donut3339 <39208955+Donut3339@users.noreply.github.com>
2020-09-04 12:15:07 +0700

    Update role-claim.js

--- role-claim.js
+++ role-claim.js
@@ -7,7 +7,7 @@ module.exports = (client) => {
         client.emojis.cache.find((emoji) => emoji.name === emojiName)
 
     const emojis = {
-        o: 'Notification',
+        notification: 'o',
     }
 
     const reactions = []
